a51adaeb8771690648801f865d01f1a6eabe6795
Zac Allen <zachary.allen2@snhu.edu>
2024-12-06 13:05:31 -0500

    Completed Day 2

--- Day - 2/errorchecking_errortype_errorconversion.py
+++ Day - 2/errorchecking_errortype_errorconversion.py
@@ -1,4 +1,25 @@
 # Type Error, Type Checking, Type Conversion
 
 # type error
-# len(12345) gives Type Error: invalid syntax
+# len(12345) Type Error: object of type 'int' has no len()
+len("Hello")  # changed 12345 to Hello to clear Type Error
+
+print(type("Hello"))  # type <class 'str'>
+print(type(12345))  # type <class 'int'>
+print(type(3.1459))  # type <class 'float>
+print(type(True))  # type <class 'bool'>
+
+
+# type conversion
+print("123" + "456")
+
+print(int(123) + int(456))
+
+# print(int("abc") + int("456")) ValueError: invalid literal for int() with base 10: 'abc'
+
+# Make the below line of code to run without errors
+#print("Number of letters in your name: " + (len(input("Enter your name"))))
+
+user_name = input("Enter your name: ")
+user_name_length = len(user_name)
+print("Number of letters in your name: " + str(user_name_length))
---
+++ Day - 2/mathematical_operations.py
@@ -0,0 +1,16 @@
+# Learn to use the basic mathematical operators: +, -, *, /, and **
+# PEMDAS: Parenthesis, Exponents, Multiplication, Division, Addition, Subtraction
+print("My age: " + str(12))
+
+print(123 + 123)  # addition
+print(7 - 3)  # subtraction
+print(3 * 2)  # multiplication
+print(6 / 3)  # division
+print(5 // 3)  # no remainder division 1.6666 becomes 1
+print(12 ** 12)  # exponent
+
+# PAUSE 1: What is the output of the code below?
+print(3 * 3 + 3 / 3 - 3)  # (3 * 3 = 9) (3 / 3 = 1) (9 + 1 = 10) (10 - 3 = 7)
+
+# PAUSE 2: Change the code below, so it outputs 3
+print(3 * 3 // 3 + 3 - 3)  # (3 * 3 = 9) (9 // 3 = 3) (3 + 3 = 6) (6 - 3 = 3)
